f72efa820712853cc97ebc34472d99b2dcacceb2
nayencoder <96290548+nayencoder@users.noreply.github.com>
2023-04-15 15:47:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function startClassification()
+{
+navigator.mediaDevices.getUserMedia({audio:true});
+classifier = ml5.soundClassifier('',modelReady);
+
+}
+
+function modelReady()
+{
+classifier.classify(gotResults);
+}
